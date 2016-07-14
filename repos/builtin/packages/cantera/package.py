Mattlk13/spack

cantera: use nolink for python module dependencies
Unsure about this, but this is probably true.

var/spack/repos/builtin/packages/cantera/package.py
@@ -52,10 +52,10 @@ class Cantera(Package):
 
     # Python module dependencies
     extends('python', when='+python')
-    depends_on('py-numpy',  when='+python')
-    depends_on('py-scipy',  when='+python')
-    depends_on('py-cython', when='+python')
-    depends_on('py-3to2',   when='+python')
+    depends_on('py-numpy',  when='+python', deptypes=nolink)
+    depends_on('py-scipy',  when='+python', deptypes=nolink)
+    depends_on('py-cython', when='+python', deptypes=nolink)
+    depends_on('py-3to2',   when='+python', deptypes=nolink)
     # TODO: these "when" specs don't actually work
     # depends_on('py-unittest2',     when='+python^python@2.6')
     # depends_on('py-unittest2py3k', when='+python^python@3.1')

daxzio/spack-packages

repos/builtin/packages/cantera/package.py
@@ -52,10 +52,10 @@ class Cantera(Package):
 
     # Python module dependencies
     extends('python', when='+python')
-    depends_on('py-numpy',  when='+python')
-    depends_on('py-scipy',  when='+python')
-    depends_on('py-cython', when='+python')
-    depends_on('py-3to2',   when='+python')
+    depends_on('py-numpy',  when='+python', type=nolink)
+    depends_on('py-scipy',  when='+python', type=nolink)
+    depends_on('py-cython', when='+python', type=nolink)
+    depends_on('py-3to2',   when='+python', type=nolink)
     # TODO: these "when" specs don't actually work
     # depends_on('py-unittest2',     when='+python^python@2.6')
     # depends_on('py-unittest2py3k', when='+python^python@3.1')